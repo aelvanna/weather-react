8516e3b382c92b772c002b5ca426f52f743da336
aelvanna <lmarie.fernandez@gmail.com>
2023-02-02 15:23:51 +1000

    removed unnecessary code

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 import App from "./App";
-import Weather from "./Weather";
 import Footer from "./Footer";
 import reportWebVitals from "./reportWebVitals";
 
