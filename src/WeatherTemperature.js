859e3d51cc949252179ad0c98880122cd262ec60
aelvanna <lmarie.fernandez@gmail.com>
2023-02-11 11:52:16 +1000

    removed code for converting Celsius to Fahrenheit and vice versa

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -2,46 +2,12 @@ import React, { useState } from "react";
 import "./App.css";
 
 export default function WeatherTemperature(props) {
-  const [unit, setUnit] = useState("celsius");
-
-  function convertToFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-
-  function convertToCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-
-  if (unit === "celsius") {
-    return (
-      <div className="WeatherTemperature">
-        <span className="temperature">
-          <strong>{Math.round(props.celsius)}</strong>
-        </span>
-        <span className="unit">
-          °C |{" "}
-          <a href="/" onClick={convertToFahrenheit}>
-            °F
-          </a>
-        </span>
-      </div>
-    );
-  } else {
-    let fahrenheit = (props.celsius * 9) / 5 + 32;
-    return (
-      <div className="WeatherTemperature">
-        <span className="temperature">
-          <strong>{Math.round(fahrenheit)}</strong>
-        </span>
-        <span className="unit">
-          <a href="/" onClick={convertToCelsius}>
-            °C
-          </a>{" "}
-          | °F
-        </span>
-      </div>
-    );
-  }
+  return (
+    <div className="WeatherTemperature">
+      <span className="temperature">
+        <strong>{Math.round(props.celsius)}</strong>
+      </span>
+      <span className="unit">°C</span>
+    </div>
+  );
 }
